1b78fc2acb1bd9b4e40378fa333360bd9e10e6e0
Darsh-Bothra <darshbothra007@gmail.com>
2024-09-13 14:14:49 +0530

    Added search filters

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -6,36 +6,52 @@ export const createBlog = async (req, res) => {
         const data = await Blog.create(req.body);
         res.json(data);
     } catch (error) {
-        res.status(500).json({message: err.message});
+        res.status(500).json({ message: err.message });
     }
 }
 
-export const getBlogAll = async(req, res) => {
+export const getBlogAll = async (req, res) => {
     try {
-        const data = await Blog.find();
-        res.json(data);
+        const { title, author, category } = req.query;
+        const queryObject = {};
+        if (title) {
+            queryObject.title = {$regex: title, $options: 'i'};
+            console.log(queryObject.title);
+        }
+        if (author) {
+            // queryObject.author = {$regex: author, $options: 'i'};
+            queryObject.author = author;
+            
+            console.log(queryObject.title);
+        }
+        if (category) {
+            queryObject.category = category;
+            console.log(queryObject.category);
+        }
+        const data = await Blog.find(queryObject);
+        return res.status(200).json(data);
     } catch (error) {
-        res.status(500).json({message: err.message});
+        res.status(500).json({ message: err.message });
     }
 };
 
-export const getBlog = async(req, res) => {
+export const getBlog = async (req, res) => {
     try {
         const data = await Blog.findById(req.params.id);
-        if(!data) {
+        if (!data) {
             return res.status(404).json("User not found");
         }
         res.json(data);
     } catch (error) {
-        res.status(500).json({message: err.message});
+        res.status(500).json({ message: err.message });
     }
 };
 
 
-export const updateBlog = async(req, res) => {
+export const updateBlog = async (req, res) => {
     const user = await Blog.findById(req.params.id);
-    try {   
-        if(!user) {
+    try {
+        if (!user) {
             return res.status(404).json("User not found");
         }
         const data = await Blog.findByIdAndUpdate(req.params.id, req.body, {
@@ -43,18 +59,18 @@ export const updateBlog = async(req, res) => {
         });
         res.json(data);
     } catch (error) {
-        res.status(500).json({message: err.message});
+        res.status(500).json({ message: err.message });
     }
 }
 
-export const deletBlog = async(req, res) => {
+export const deletBlog = async (req, res) => {
     try {
         const data = await Blog.findByIdAndDelete(req.params.id)
-        if(!data) {
+        if (!data) {
             return res.status(404).json("User not found");
         }
-        res.json({messaga: "Blog deleted successfully", deletedBlog: data})
+        res.json({ messaga: "Blog deleted successfully", deletedBlog: data })
     } catch (error) {
-        res.status(500).json({message: err.message});
+        res.status(500).json({ message: err.message });
     }
 }
